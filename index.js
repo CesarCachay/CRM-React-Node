3b5671621cf89e17c965bbb1b135048adfbaaabb
CesarCachay <cesarcachay1996@gmail.com>
2020-10-03 16:15:30 -0500

    Project setup 🎉

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+// Creating the server
+const app = express();
+const PORT = 5000;
+
+// Port
+app.listen(PORT);
+console.log(`Server running on port ${PORT}`);
